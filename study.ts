323e2b5e11c5305deedd6e195d1ea4c2d6a710ec
kim-29 <nameofwind@gmail.com>
2021-12-26 08:28:27 +0900

    1 file modified

--- study.ts
+++ study.ts
@@ -3,3 +3,15 @@ import './style.css';
 
 const appDiv: HTMLElement = document.getElementById('appy');
 appDiv.innerHTML = `<h1>TypeScript</h1>`;
+
+function myfn1(arg1) {
+  return function myfn2(arg2) {
+    return arg2 + 10;
+  };
+}
+
+let result1 = myfn1(5);
+let result2 = myfn1(5)(10);
+
+console.log(result1);
+console.log(result2);
